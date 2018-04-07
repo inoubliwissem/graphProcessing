e925eac32a55a2da34f01cd0921f5d81ac482505
inoubliwissem <inoubliwissem@gmail.com>
2018-04-07 22:45:41 +0100

    .

--- Clustering.py
+++ Clustering.py
@@ -1,11 +1,36 @@
-#we implement th SCAN alrorithm to graph clustering
-#modilarity function
-#in this function we make a a set of tribplet (node1, node2, 0) 
-# and update this liste to calculate the similarity between okk tupe in the list
-def modilarity(triplets):
+from math import sqrt
+#We implement th SCAN alrorithm to graph clustering
+#Modilarity Function
+#In this function we make a a set of tribplet (node1, node2, 0) 
+# And update this liste to calculate the similarity between okk tupe in the list
+def modilarity(triplets,neighbors):
    result=[]
-   result.append(triplets.get(0))
+   #We calculate the similarity between tow nodes, and push this into triplets liste to update zero value 
+   for elm in triplets:
+      n1=elm[0]
+      n2=elm[1]
+      print neighbors[n1]
+      n=(n1,n2,2)
+      result.append(n)
    return result
+#this function to get a list of neighbors foreach node
+#it takes a set of edges, and produce a list of couple "node: [list of neighbors]"
+def getNeighbors(edges):
+   neighbors={}
+   for e in edges:
+   	   a,b=e
+   	   if neighbors.has_key(a):
+   	        neighbors[a].append(b)
+   	   else:
+   	    	l=[b]
+   	    	neighbors[a]=l
+   	   if neighbors.has_key(b):
+   	    	neighbors[a].append(a)
+   	   else:
+   	    	l=[a]
+   	    	neighbors[b]=l
+   return neighbors
+
 if __name__=='__main__' :
    #load the graph from text file
    dataset=open("dataset","r")
@@ -34,4 +59,9 @@ if __name__=='__main__' :
          if i<j:
             t=(i,j,0)
             triplets.append(t)
-   print modilarity(triplets)
+   ng=getNeighbors(e)
+   print modilarity(triplets,ng)
+   
+   #print set(ng[2]+ng[1])
+   
+   
