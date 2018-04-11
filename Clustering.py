383d086372f36a9c91489c7d4b5017f8e881cbb4
wissem inoubli <inoubliwissem@gmail.com>
2018-04-11 17:28:31 +0100

    11042018

--- Clustering.py
+++ Clustering.py
@@ -2,7 +2,7 @@ from math import sqrt
 #We implement th SCAN alrorithm to graph clustering
 #Modilarity Function
 #In this function we make a a set of tribplet (node1, node2, 0) 
-# And update this liste to calculate the similarity between okk tupe in the list
+#And update this liste to calculate the similarity between okk tupe in the list
 def modilarity(triplets,neighbors):
    result=[]
    #We calculate the similarity between tow nodes, and push this into triplets liste to update zero value 
@@ -14,8 +14,8 @@ def modilarity(triplets,neighbors):
       n=(n1,n2,sim)
       result.append(n)
    return result
-#this function to get a list of neighbors foreach node
-#it takes a set of edges, and produce a list of couple "node: [list of neighbors]"
+#This function to get a list of neighbors foreach node
+#It takes a set of edges, and produce a list of couple "node: [list of neighbors]"
 def getNeighbors(edges):
    neighbors={}
    for e in edges:
@@ -26,7 +26,7 @@ def getNeighbors(edges):
    	    	l=[b]
    	    	neighbors[a]=l
    	   if neighbors.has_key(b):
-   	    	neighbors[a].append(a)
+   	    	neighbors[b].append(a)
    	   else:
    	    	l=[a]
    	    	neighbors[b]=l
@@ -39,7 +39,7 @@ def getStrongNeighbors(similarity,n,e):
    	return nb   	   	  
 
    #In this function we get a set of core node according the epsilon and lambda values
-   # it takes the similarity triplets and the neighbors dictionnary
+   #it takes the similarity triplets and the neighbors dictionnary
 def getCore(simlarity, neighbors,e,l):
 	nodes=neighbors.keys()
 	c=[]
@@ -56,15 +56,15 @@ def getNeighborsOfCore(cluster,neighbors):
 			neighbor.append(n)
 	return neighbor
 if __name__=='__main__' :
-   #load the graph from text file
+   #Load the graph from text file
    dataset=open("dataset2","r")
-   #get all lines of the dataset
+   #Get all lines of the dataset
    lines=dataset.readlines()
-   #dictionary to save for each vertices its neighbors
+   #Dictionary to save for each vertices its neighbors
    adj={}
-   #liste of edges
+   #Liste of edges
    e=[]
-   # fill the dictionary and the list
+   #Fill the dictionary and the list
    for l in lines:
      pair=l.strip().split("\t")
      if len(pair)>1 :
@@ -93,17 +93,21 @@ if __name__=='__main__' :
    clusters=[]
    i=0
    clusters.append([cores[0]])
-   cores.remove(cores[0])
-   while(len(cores)>1):
-   	  n=cores[0]
-   	  added=0
-   	  for c in clusters:
-   	  	neighCores=getNeighborsOfCore(c,ng)
-   	  	if n in neighCores:
-   	  		c.append(n)
-   	  		cores.remove(n)
-   	  		added=1
-   	  	if added==1:
-   	  		break
+   #cores.remove(cores[0])
+   #show all cores nodes
+   while(len(cores)>0):
+       n=cores[0]
+       #for each node we get all it cluster
+       neighbors_n=getNeighborsOfCore([n],ng)
+       cluster=[]
+       for m in neighbors_n:
+           if n in cores:
+               cluster.append(m)
+               #cores.remove(cores[m])
+       clusters.append(cluster)
+               
+       cores.remove(cores[0])
+   print clusters
+     
 
    	   
